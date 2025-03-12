e2fde766630823f9e004b02efb151d77c1d341e5
Dave Kerr <dwmkerr@gmail.com>
2025-03-12 12:56:21 +0000

    build: fix workflow (#5)
    * build: fix workflow
    
    * fix build
    
    * build: node workflow
    
    * fix build
    
    * fix build
    
    * fix build
    
    * fix build
    
    * fix build
    
    * fix build
    
    * fix build
    
    * chore: release 0.1.0
    
    Release-As: 0.1.0
    
    * chore: release 0.1.0
    
    Release-As: 0.1.0
    
    * chore: release 0.1.0
    
    Release-As: 0.1.0
    
    * fix build

--- modules/node/src/models.ts
+++ modules/node/src/models.ts
@@ -1,6 +1,26 @@
-import fs from 'fs';
-import path from 'path';
-import YAML from 'yaml';
+import fs from "fs";
+import path from "path";
+import YAML from "yaml";
+
+export interface Pricing {
+  per_million_input_tokens?: number;
+  per_million_output_tokens?: number;
+}
+
+export interface Providers {
+  version: string;
+  updated: string;
+  source: string;
+  author: string;
+  providers: Provider[];
+}
+
+export interface Provider {
+  name: string;
+  docs: string;
+  api_specification: string;
+  models: Model[];
+}
 
 // Exported TypeScript type definition (simplified, extend as needed)
 export interface Model {
@@ -12,19 +32,19 @@ export interface Model {
   short_description: string;
   modalities: Record<string, boolean>;
   endpoints: Record<string, boolean>;
-  price?: {
-    per_million_input_tokens?: number;
-    per_million_output_tokens?: number;
-  };
+  price?: Pricing;
   context_window_tokens: number;
   max_output_tokens: number;
 }
 
 // Models.yaml as JS object
 export const modelsData = YAML.parse(
-  fs.readFileSync(path.join(__dirname, '../../models.yaml'), 'utf8')
+  fs.readFileSync(path.join(__dirname, "../../models.yaml"), "utf8"),
 );
 
-// Array of models exported for convenience
-export const models: Model[] = modelsData.providers.flatMap((provider: any) => provider.models);
+export const providersData: Providers = YAML.parse(
+  fs.readFileSync(path.join(__dirname, "../../models.yaml"), "utf8"),
+);
 
+// Convenient named exports
+export const providers: Provider[] = providersData.providers;
