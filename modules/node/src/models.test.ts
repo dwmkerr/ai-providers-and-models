200a481ce23a02a17a7b11bedc66968bff5a0231
Dave Kerr <dwmkerr@gmail.com>
2025-03-13 12:05:13 +0000

    feat: 03 mini, 4o, 4o-mini

--- modules/node/src/models.test.ts
+++ modules/node/src/models.test.ts
@@ -6,7 +6,7 @@ describe("models", () => {
       const openai = providers["openai"];
       expect(openai).not.toBeUndefined();
       expect(openai.name).toBe("OpenAI");
-      expect(Object.keys(openai.models).length).toBe(4);
+      expect(Object.keys(openai.models).length).toBe(6);
     });
   });
 
@@ -31,11 +31,35 @@ describe("models", () => {
       });
     });
 
+    describe("o3-mini-2025-01-31", () => {
+      test("is configured", () => {
+        const model = providers["openai"].models["o3-mini-2025-01-31"];
+        expect(model).not.toBeUndefined();
+        expect(model.id).toBe("o3-mini-2025-01-31");
+      });
+    });
+
     describe("o1-2024-12-17", () => {
       test("is configured", () => {
         const model = providers["openai"].models["o1-2024-12-17"];
         expect(model).not.toBeUndefined();
         expect(model.id).toBe("o1-2024-12-17");
+      });
+    });
+
+    describe("gpt-4o-mini-2024-07-18", () => {
+      test("is configured", () => {
+        const model = providers["openai"].models["gpt-4o-mini-2024-07-18"];
+        expect(model).not.toBeUndefined();
+        expect(model.id).toBe("gpt-4o-mini-2024-07-18");
+      });
+    });
+
+    describe("gpt-4-0613", () => {
+      test("is configured", () => {
+        const model = providers["openai"].models["gpt-4-0613"];
+        expect(model).not.toBeUndefined();
+        expect(model.id).toBe("gpt-4-0613");
       });
     });
   });
